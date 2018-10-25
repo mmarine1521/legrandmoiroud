85db5e80cb5c0c8fa7cb277f2750d026f192f1ef
KvLegrand <kv.legrand@gmail.com>
2018-10-25 20:25:21 +0200

    2.1

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -537,7 +537,7 @@ int main(int argc,char* argv[])
     std::cout << "pays : " << carte1.getPays() << "  //Grande Bretagne" << std::endl;
 
 
-    sf::RenderWindow window(sf::VideoMode(6000,4000), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(1300,700), "RISK");
 
     	    while (window.isOpen())
     	    {
@@ -553,16 +553,17 @@ int main(int argc,char* argv[])
     	    {
     	    //erreur
     	    }
+
     	    texture2.loadFromFile("res/tank384px.png");
     	    texture.setSmooth(true);
     	    texture2.setSmooth(true);
     	    sf::Sprite sprite ;
     	    sf::Sprite sprite2 ;
     	    sprite.setTexture(texture);
-    	    sprite.setScale(sf::Vector2f(2.f, 2.f));
+    	    sprite.setScale(sf::Vector2f(1.5f, 1.5f));
     	    sprite2.setTexture(texture2);
     	    sprite2.setScale(sf::Vector2f(0.08f, 0.08f));
-    	    sprite2.setPosition(sf::Vector2f(335.f, 659.f));
+    	    sprite2.setPosition(sf::Vector2f(245.f, 500.f));
 
     	        window.clear();
     	        window.draw(sprite);
---
+++ src/client/render/MobileTileSet.cpp
@@ -0,0 +1,8 @@
+#include "TileSet.h" 
+
+//MobileTileSet ();
+//    ~MobileTileSet ();
+//    int const getCellWidth ();
+//    int const getCellHeight ();
+//    const std::string getImageFile () const;
+//    const Tile& getTile (const state::Element& e) const;
