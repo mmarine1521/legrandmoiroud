5af2756d18da9b013e36ff01b66c62bdd14c417d
Marine Moiroud <marine.moiroud@orange.fr>
2018-11-12 17:26:12 +0100

    debut IA

---
+++ src/shared/ai/RandomAI.cpp
@@ -0,0 +1,21 @@
+#include "RandomAI.h"
+
+namespace ai {
+
+//void RandomAI::aiDistribution (state::State state, int nbJoueurs){}
+
+//bool RandomAI::aiRepartitionArmees (int idJoueur, state::State state){}
+
+//std::string RandomAI::aiChoixPaysAttaquant (){}
+
+//std::string RandomAI::aiChoixPaysAttaque (){}
+
+//int RandomAI::aiNbDesLancers (){}
+
+//int RandomAI::aiEchange (int idJoueur, state::State state){}
+
+//void RandomAI::aiPlacerNouvellesArmees (int idJoueur, int nouvellesArmees, state::State state){}
+
+//void RandomAI::aiDeplacerArmees (int idJoueur, state::State state){}
+
+}
