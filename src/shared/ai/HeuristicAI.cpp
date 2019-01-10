0366a86b7eb7155caf0d92f86296333f90b55689
Marine Moiroud <marine.moiroud@orange.fr>
2019-01-10 08:37:31 +0100

    ai

---
+++ src/shared/ai/HeuristicAI.cpp
@@ -0,0 +1,16 @@
+#include "HeuristicAI.h"
+
+namespace ai{
+
+void aiPlacementArmees (state::State state){
+  
+}
+//void aiChoixPaysAttaquant (std::vector<std::string> blackList, state::State state);
+//void aiChoixPaysAttaque (state::State state);
+//void aiDesAttaquant (state::State state);
+//void aiDesAttaque (state::State state);
+//void aiDefausser (state::State state);
+//void aiEchange (state::State state);
+//void aiDeplacerArmees (state::State state);
+
+}
